6c4130ffe761c760bed903316fd9e60c5a113932
Rishbhmisirpur <40860799+Rishbhmisirpur@users.noreply.github.com>
2026-04-18 17:21:23 +0530

    Refactor scraper.py to improve readability

--- scraper.py
+++ scraper.py
@@ -3,9 +3,7 @@ from bs4 import BeautifulSoup
 
 def get_html(url):
     try:
-        headers = {
-            "User-Agent": "Mozilla/5.0"
-        }
+        headers = {"User-Agent": "Mozilla/5.0"}
         res = requests.get(url, headers=headers, timeout=10)
         return res.text
     except:
