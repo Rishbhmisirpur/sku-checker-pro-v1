0679104f83a74362b553d9039eb334980547e7e6
Rishbhmisirpur <40860799+Rishbhmisirpur@users.noreply.github.com>
2026-04-18 20:04:00 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,20 +1,24 @@
 import requests
-from bs4 import BeautifulSoup
+import random
+
+session = requests.Session()
 
 def get_html(url):
     try:
-        headers = {"User-Agent": "Mozilla/5.0"}
-        res = requests.get(url, headers=headers, timeout=10)
-        return res.text
-    except:
+        headers_list = [
+            {"User-Agent": "Mozilla/5.0"},
+            {"User-Agent": "Chrome/124"},
+            {"User-Agent": "Safari/537"}
+        ]
+
+        for _ in range(3):
+            headers = random.choice(headers_list)
+            r = session.get(url, headers=headers, timeout=25)
+
+            if r.status_code == 200 and len(r.text) > 200:
+                return r.text
+
         return ""
 
-def extract_image(html):
-    try:
-        soup = BeautifulSoup(html, "html.parser")
-        img = soup.find("img")
-        if img:
-            return img.get("src") or img.get("data-src") or ""
-        return ""
     except:
         return ""
