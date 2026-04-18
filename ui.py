bd9640f06a12b2c4aaf7a8b142359e7fe712b591
Rishbhmisirpur <40860799+Rishbhmisirpur@users.noreply.github.com>
2026-04-18 20:44:55 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,4 +1,3 @@
-# ui.py
 import streamlit as st
 
 
@@ -8,13 +7,25 @@ def chat_support():
     msg = st.sidebar.text_input("Ask issue")
 
     if st.sidebar.button("Send"):
-        st.sidebar.success("We received your request")
+        st.sidebar.success("Request received")
 
 
 def show_metrics(df):
     st.subheader("📊 Summary")
-    st.write(df["result"].value_counts())
+
+    if "final_result" in df.columns:
+        st.write(df["final_result"].value_counts())
+
+    if "sku_match" in df.columns:
+        st.write(df["sku_match"].value_counts())
+
+    if "seller_match" in df.columns:
+        st.write(df["seller_match"].value_counts())
+
+    if "price_match" in df.columns:
+        st.write(df["price_match"].value_counts())
 
 
 def show_chart(df):
-    st.bar_chart(df["result"].value_counts())
+    if "final_result" in df.columns:
+        st.bar_chart(df["final_result"].value_counts())
