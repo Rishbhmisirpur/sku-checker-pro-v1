7a4329e07b2b80350cb13f7141d1c3c3ddc156fd
Rishbhmisirpur <40860799+Rishbhmisirpur@users.noreply.github.com>
2026-04-18 20:03:35 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,11 @@
+import sqlite3
 
+def save_result(sku, seller, price, result):
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+
+    c.execute("INSERT INTO results (sku, seller, price, result) VALUES (?, ?, ?, ?)",
+              (sku, seller, price, result))
+
+    conn.commit()
+    conn.close()
